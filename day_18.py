c64b3855bb79e248eff744bb5531b878d7427350
Sarthak Sharma <sarthaksharma1105@gmail.com>
2024-07-29 12:32:03 +0530

    add chatgpt version also.

--- day_18.py
+++ day_18.py
@@ -28,3 +28,31 @@ while True:
     continue
 
 print("Good job")
+
+
+# number = int(input("Enter a number; 1 to 1000: "))
+
+# while True:
+#     guess = int(input("Guess the number: "))
+
+#     if guess == number:
+#         print("You guessed it!")
+#         break
+
+#     elif abs(guess - number) >= 100:
+#         print("Hundreds off!")
+#         print("Guess again!")
+
+#     elif abs(guess - number) >= 50:
+#         print("Half a century far")
+#         print("Guess again!")
+
+#     elif abs(guess - number) >= 25:
+#         print("Quarter century far")
+#         print("Guess again!")
+
+#     elif abs(guess - number) >= 10:
+#         print("Hot hot hot, only 10 paces off")
+#         print("Guess again! Almost there")
+
+# print("Good job")
