45332b25ae4f6fa9be4c4afeeb02ed5464a8f6aa
Sarthak Sharma <sarthaksharma1105@gmail.com>
2024-08-06 13:08:11 +0530

    completed package assignment

---
+++ bootcamp/python/library/packagessolution/greetings/__init__.py
@@ -0,0 +1,2 @@
+# __init__.py
+from .greets import say_hello, lucky_number
---
+++ bootcamp/python/library/packagessolution/greetings/greets.py
@@ -0,0 +1,10 @@
+def say_hello():
+    
+    user_name = input("Hi, may I know your name? ")
+    
+    return f"Hi there {user_name}, Lets get started."
+
+def lucky_number():
+    user_number = input("Please write your lucky number? ")
+    return (f"Ok {user_number} is good.")
+        
